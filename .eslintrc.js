3d130836ca901e8ca55424a07d02a9c5563fc607
Guillermo de la Iglesia <zguillez@gmail.com>
2020-03-14 20:03:04 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  "extends": "google",
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module"
+  'extends': 'google',
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
   },
-  "rules": {
-    "max-len": "off",
-    "no-restricted-modules": "off"
+  'rules': {
+    'max-len': 'off',
+    'no-restricted-modules': 'off',
+    'valid-jsdoc': 'off',
+    'guard-for-in': 'off',
   },
-  "plugins": [
-    "html"
-  ]
+  'plugins': [
+    'html',
+  ],
 };
--- bin/test.js
+++ bin/test.js
@@ -1,21 +1,18 @@
 #!/usr/local/bin/node
-const path = require('path');
+/* const path = require('path');
 const zssh = require('../src/Zssh');
 zssh.config = path.resolve(__dirname, './ssh.json');
 zssh.download().then(() => {
   console.log(`Download is done`.green);
   zssh.close();
-}).catch(err => {
+}).catch((err) => {
   console.log(`${err}`.red);
   zssh.close();
-});
-/*zssh.upload().then(() => {
+});*/
+/* zssh.upload().then(() => {
  console.log(`Upload is done`.green);
  zssh.close();
  }).catch(err => {
  console.log(`${err}`.red);
  zssh.close();
- });*/
-
-
-
+ }); */
